eb819847c06cafd0c159259914f4a616614a2b07
Satya P Javvadi <saphin.rao@gmail.com>
2019-08-12 15:29:38 -0700

    Add project files.

---
+++ AddressBook/Address.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    class Address
+    {
+        private const string TEXT_FILE_NAME = "Address.txt"; 
+        public string Name { get; set; }
+        public string EmailAddress { get; set; }
+        public string PhoneNumber { get; set; }
+        public static void writeAddress(Address address)
+        {
+            var addressData = $"{address.Name}, {address.EmailAddress}, {address.PhoneNumber}";
+            FileHelper.WriteTextFile(TEXT_FILE_NAME, addressData);
+
+        }
+    }
+}
---
+++ AddressBook/FileHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.Storage.Streams;
+
+namespace AddressBook
+{
+    public static class FileHelper
+    {
+        public async static void WriteTextFile(string filename, string content)
+        {
+            var storageFolder = ApplicationData.Current.LocalFolder;
+            var textFile = await storageFolder.CreateFileAsync(filename, CreationCollisionOption.OpenIfExists);
+            var textStream = await textFile.OpenAsync(FileAccessMode.ReadWrite);
+            var textWriter = new DataWriter(textStream);
+            textWriter.WriteString(content);
+            await textWriter.StoreAsync();
+        }
+    }
+}
